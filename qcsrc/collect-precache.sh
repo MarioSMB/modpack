b480b1f1326e0a3daf582a84ac9e4b9276b246ab
Mario <mario.mario@y7mail.com>
2014-06-05 10:04:20 +1000

    It's time for another re-clone

--- qcsrc/collect-precache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-{
-	grep -h '\<precache_model *( *"' client/* | grep -v "//NO_SV_PRECACHE"
-	grep -h '\<precache_sound *( *"' client/* | grep -v "//NO_SV_PRECACHE"
-} > server/precache-for-csqc.inc
-
