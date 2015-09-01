aaaf67050d35764c3f7a93985bc8311168cb2190
TimePath <andrew.hardaker1995@gmail.com>
2015-09-01 15:50:40 +1000

    Changelog

--- mod/sync.sh
+++ mod/sync.sh
@@ -1,31 +1,57 @@
 #!/bin/sh
 cd ${0%[\\/]*}
 
-if [ ! -d "github" ]; then
-    mkdir github
-    pushd github
+SYNCDIR=github
+SYNCREMOTE=git@github.com:MarioSMB/modpack.git
+SYNCBRANCH=master
+
+if [ ! -d "${SYNCDIR}" ]; then
+    mkdir "${SYNCDIR}"
+    pushd "${SYNCDIR}" > /dev/null
     git init
-    git remote add origin git@github.com:MarioSMB/modpack.git
+    git remote add origin "${SYNCREMOTE}"
     git fetch
-    git checkout -b master
-    popd
+    git checkout -b "${SYNCBRANCH}"
+    popd > /dev/null
 fi
 
-cat << EOF > github/.gitignore
-.DS_Store
-EOF
+function copy {
+    src="${1}"
+    dst="${SYNCDIR}/${2}"
+    mkdir -p "$(dirname "${dst}")"
+    git show "HEAD:./${src}" > "${dst}"
+}
 
-cp README.md github
+function prune {
+    rm "${SYNCDIR}/${1}"
+}
 
-rsync -Pha --delete \
-    --exclude 'github' --exclude 'sync.sh' --exclude '.git' --exclude '.gitignore' \
-    ../mod ../qcsrc \
-    github
+pushd .. > /dev/null
+FILES=$(git ls-tree --name-only -r HEAD | egrep '^(mod|qcsrc)/')
+popd > /dev/null
+
+pushd "${SYNCDIR}" > /dev/null
+git rm -rf . > /dev/null
+git clean -xdf
+popd > /dev/null
+
+for f in ${FILES}; do copy "../${f}" "${f}"; done
+
+prune "mod/sync.sh"
+prune "mod/.gitignore"
+prune "mod/README.md"
+copy "../.gitignore" ".gitignore"
+copy "README.md" "README.md"
 
 REV=$(git describe)
+LASTREV=$(git rev-parse "$(cd "${SYNCDIR}"; git log --format=%B -n 1 HEAD | head -n 1)")
+CHANGES=$(git log --format=%B --reverse ${LASTREV}..HEAD)
+NL='
+'
+MSG="Sync with ${REV}${NL}${NL}${CHANGES}"
 
-pushd github
+pushd github > /dev/null
 git add .
-git commit -m "Sync with ${REV}"
-git push -u origin master
-popd
+git commit -m "${MSG}"
+git push -u origin "${SYNCBRANCH}"
+popd > /dev/null
