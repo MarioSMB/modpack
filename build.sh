6295b96643521144df0fe6fe09a228f7c3cff5cf
TimePath <andrew.hardaker1995@gmail.com>
2015-10-04 22:55:14 +1100

    ./update.sh: build gmqcc from source

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ if [ ! -d "$base" ]; then
     echo "RTFM (README.md)"
     exit 1
 fi
-: ${QCC:=$PWD/gmqcc}
+: ${QCC:=$PWD/gmqcc/gmqcc}
 export QCC
 cd ${base} && make BUILD_MOD=1
--- update.sh
+++ update.sh
@@ -5,9 +5,22 @@ declare repository="$(git config --get remote.origin.url)"
 declare defaultbranch='xonotic-data.pk3dir/master'
 declare base='xonotic'
 
+declare qccDir='gmqcc'
+declare qccRepo='https://github.com/graphitemaster/gmqcc.git'
+declare qccBranch='master'
+
 git pull
+
 if [ ! -d "$base" ]; then
     git clone ${repository} -b ${defaultbranch} ${base}
 else
     cd ${base} && git pull
 fi
+
+if [ ! -d "$qccDir" ]; then
+    git clone ${qccRepo} -b ${qccBranch} ${qccDir}
+    cd ${qccDir}
+else
+    cd ${qccDir} && git pull
+fi
+make
