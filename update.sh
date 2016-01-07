ca6f77747f354514138f119ded2a82992b428f7c
TimePath <andrew.hardaker1995@gmail.com>
2016-01-07 20:24:20 +1100

    Switch to submodules

--- update.sh
+++ update.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 cd ${0%[\\/]*}
 set -eu
-declare repository="$(git config --get remote.origin.url)"
-declare defaultbranch='xonotic-data.pk3dir/master'
-declare base='xonotic'
+
+git submodule update --init --depth 100
 
 declare qccDir='gmqcc'
 declare qccRepo='https://github.com/graphitemaster/gmqcc.git'
 declare qccBranch='master'
 
 git pull
-
-if [ ! -d "$base" ]; then
-    git clone ${repository} -b ${defaultbranch} ${base}
-else
-    pushd ${base} && git pull
-    popd
-fi
 
 if [ ! -d "$qccDir" ]; then
     git clone ${qccRepo} -b ${qccBranch} ${qccDir}
