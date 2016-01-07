b8e2c8588911982af584a50e740921c6bef76537
TimePath <andrew.hardaker1995@gmail.com>
2016-01-08 10:18:31 +1100

    Fix output directory

--- build.sh
+++ build.sh
@@ -7,7 +7,13 @@ if [ ! -d "$base" ]; then
     echo "RTFM (README.md)"
     exit 1
 fi
+
+: ${PROGS_OUT:=$PWD}
+export PROGS_OUT
+
 : ${QCC:=$PWD/gmqcc/gmqcc}
 export QCC
+
 export QCCFLAGS_WATERMARK=$(git describe)
+
 cd ${base} && make BUILD_MOD=1
