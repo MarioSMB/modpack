724deb24eb8cc394c1c8abade72275714e72c0c6
TimePath <andrew.hardaker1995@gmail.com>
2016-01-08 21:40:03 +1100

    Travis: do ./update.sh

--- update.sh
+++ update.sh
@@ -5,19 +5,21 @@ set -eu
 git submodule update --init --depth 100
 
 declare qccDir='gmqcc'
-declare qccRepo='https://github.com/graphitemaster/gmqcc.git'
+declare qccRepo='https://gitlab.com/xonotic/gmqcc.git'
 declare qccBranch='master'
 
 git pull --tags
 
-wget -P .cache https://github.com/MarioSMB/csprogs/raw/master/csprogs-$(git describe --tags --dirty=*).pk3
+if [ -z "${CI-}" ]; then
+    wget -P .cache https://github.com/MarioSMB/csprogs/raw/master/csprogs-$(git describe --tags --dirty=*).pk3
+fi
 
 if [ ! -d "$qccDir" ]; then
-    git clone ${qccRepo} -b ${qccBranch} ${qccDir}
+    git clone --depth=1 --branch=${qccBranch} ${qccRepo} ${qccDir}
 else
     pushd ${qccDir} && git pull
     popd
 fi
 
-pushd ${qccDir} && make
+pushd ${qccDir} && make -j $(nproc)
 popd
