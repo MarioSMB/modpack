f6140e32392ad8be157ab191046f180c7ed3e639
TimePath <andrew.hardaker1995@gmail.com>
2016-01-09 17:56:44 +1100

    update.sh: update submodules correctly

--- update.sh
+++ update.sh
@@ -2,13 +2,13 @@
 cd ${0%[\\/]*}
 set -eu
 
+git pull --tags
+
 git submodule update --init --depth 100
 
 declare qccDir='gmqcc'
 declare qccRepo='https://gitlab.com/xonotic/gmqcc.git'
 declare qccBranch='master'
-
-git pull --tags
 
 if [ -z "${CI-}" ]; then
     wget -P .cache -c https://github.com/MarioSMB/csprogs/raw/master/csprogs-$(git describe --tags --dirty=*).pk3
