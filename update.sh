f82323aa16381eb54a69f7929d0318c5816c5ef0
Mario <mario.mario@y7mail.com>
2023-01-01 18:44:23 +1000

    Update GMQCC branch to main

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ git submodule update --init --depth 100
 
 declare qccDir='gmqcc'
 declare qccRepo='https://gitlab.com/xonotic/gmqcc.git'
-declare qccBranch='master'
+declare qccBranch='main'
 
 #if [ -z "${CI-}" ]; then
 #    wget -P .cache -c https://github.com/MarioSMB/csprogs/raw/master/csprogs-$(git describe --tags --dirty=*).pk3
