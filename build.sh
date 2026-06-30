5f7b7edda1444e837be4cea9a300693486157fcd
Jiān <127670701+coolDudeRude@users.noreply.github.com>
2026-06-30 13:57:51 +0000

    Build Script and Github CI Enhancements (#60)
    * feat: add gmqcc as submodule
    
    * build: refactor build system and optimize CI workflow
    
    - Add centralized build script (build.sh)
    - Optimize GitHub Actions using caching and partial clones
    - Update .gitignore and move output directory from 'compiled/' to 'build/'
    - Remove redundant daily.yml workflow
    - Remove update.sh and integrate gmqcc compilation into build.sh
    
    * ci(build.yml): upload build artifacts
    
    * ci(build.yml): add version info the artifact name
    
    * feat: scope build output to current branch name
    
    * ci(build.yml): include branch name in artifact filenames
    
    * ci(build.yml): skip gmqcc init on cache hit
    
    * build: add flag to toggle git filter flag
    
    - Add github workflow to test building the mod against xonotic master
    
    * feat(build.sh): enhance submodule handling and CLI arguments
    
    - Add short-hand flags `-nf` and `-ni` for submodule control
    - Replace file-existence checks for submodules with `git submodule status`
    - Improve logging to include commit hashes during initialization
    - Update usage documentation
    
    * ci(daily.yml): disable build artifact upload
    
    * build: add safety flags and directory normalization to build.sh
    
    * ci(deps): bump actions/checkout from v4 to v7
    
    * docs: fix submodule clone size estimate
    
    - Use clone size instead of disk space used
    
    * ci(deps): bump actions/cache from v4 to v6

--- build.sh
+++ build.sh
@@ -1,60 +1,160 @@
 #!/usr/bin/env bash
-if [ -n "${ZIP:-}" ]; then
-	tput bold; tput setaf 3; echo "WARNING: Processing without compression..."; tput sgr0
+
+TARGET_ALL=0
+CLEAN_BUILD=0
+MAKE_TARGETS=("qc")
+FILTER_FLAGS="--filter=blob:none"
+
+if [[ "$CI" == "true" ]]; then
+  INIT_SUBMODULES=0
+else
+  INIT_SUBMODULES=1
 fi
 
-if [ -e $(dirname "$0")/compiled/csprogs.dat -o -e $(dirname "$0")/compiled/menu.dat -o -e $(dirname "$0")/compiled/progs.dat -o -e $(dirname "$0")/compiled/csprogs.lno -o -e $(dirname "$0")/compiled/menu.lno -o -e $(dirname "$0")/compiled/progs.lno -o -e $(dirname "$0")/compiled/*.pk3 -o -e $(dirname "$0")/csprogs.dat -o -e $(dirname "$0")/menu.dat -o -e $(dirname "$0")/progs.dat -o -e $(dirname "$0")/csprogs.lno -o -e $(dirname "$0")/menu.lno -o -e $(dirname "$0")/progs.lno ]; then
-    tput bold; tput setaf 3; echo "There are compiled files here. The operation will proceed to delete the current compiled files in and out of the 'compiled' directory, replace and get new compiled ones (*.dat, *.lno, and *.pk3 files will be removed)."
-    # deletes any remaining compiled files that weren't moved to 'compiled' directory
-    tput bold; tput setaf 1
-
-    rm -vf $(dirname "$0")/*.dat
-    rm -vf $(dirname "$0")/*.lno
-    rm -vf $(dirname "$0")/*.pk3
-
-    rm -vf $(dirname "$0")/compiled/*.dat
-    rm -vf $(dirname "$0")/compiled/*.lno
-    rm -vf $(dirname "$0")/compiled/*.pk3
-
-    tput sgr0
-fi
-
-cd ${0%[\\/]*}
 set -eu
 
-declare base=xonotic
-if [ ! -d "$base" ]; then
-    echo "RTFM (README.md)"
-    exit 1
-fi
-
-: ${PROGS_OUT:=$PWD}
-export PROGS_OUT
-
-: ${QCC:=$PWD/gmqcc/gmqcc}
-export QCC
-
-export QCCFLAGS_WATERMARK=$(git describe --tags --dirty='~' --always)
-
-relpath() {
-    b=; s=$(cd $(readlink -f ${1%%/}); pwd); d=$(cd $2; pwd)
-    while [ "${d#$s/}" == "${d}" ]; do s=$(dirname ${s}); b="../${b}"; done
-    echo ${b}${d#${s}/}
+usage() {
+  echo "build.sh [-c|--clean] [-a|-all] [-nf|--no-filter] [-ni|--no-init] [-h|--help]"
+  echo
+  echo " -c  | --clean          Remove compiled .dat and .pk3 file before rebuilding."
+  echo " -a  | --all            Build both qc and pk3 targets."
+  echo " -h  | --help           Show this message."
+  echo " -nf | --no-filter      Don't use --filter flag when initializing submodules."
+  echo " -ni | --no-init        Skip initializing submodules, manual initialization."
+  echo
+  echo "NOTE: Default target is 'qc' only."
 }
 
-export BUILD_MOD="$(relpath $base/qcsrc $PWD)"
-export XONOTIC=1
-make -C ${base}
-
-tput bold; tput setaf 2; echo "Compiled successfully!"; tput sgr0
-mkdir -vp $(dirname "$0")/compiled
-mv -v *.lno $(dirname "$0")/compiled
-mv -v *.dat $(dirname "$0")/compiled
-
-if [ -n "${ZIP:-}" ]; then
-    tput bold; tput setaf 3; echo "Done without compression."; tput sgr0
+# Filter repeating arguments.
+# Who would pass same arguments multiple times tho?!
+if [[ $# -gt 0 ]]; then
+  readarray -t arguments < <(printf '%s\n' "$@" | sort -u)
 else
-    cp -v $base/qcsrc/csprogs-*.pk3 ./compiled
+  # No arguments were provided
+  arguments=()
 fi
 
-tput bold; tput setaf 3; echo "Now you can look at the compiled files inside $(dirname "$0")/compiled directory."
+# Parse CLI arguments
+for i in "${arguments[@]}"; do
+  case $i in
+  -a | --all)
+    TARGET_ALL=1
+    MAKE_TARGETS+=("pk3")
+    ;;
+  -c | --clean)
+    CLEAN_BUILD=1
+    ;;
+  -h | --help)
+    usage
+    exit 0
+    ;;
+  -ni | --no-init)
+    INIT_SUBMODULES=0
+    ;;
+  -nf | --no-filter)
+    FILTER_FLAGS=""
+    ;;
+  *)
+    echo "Unknown flag: '$i' ... aborting"
+    exit 1
+    ;;
+  esac
+done
+
+cd "${0%/*}"
+
+if ! CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"; then
+  echo "=> Failed to parse git branch name... aborting"
+  exit 1
+else
+  echo "=> Current Branch is ${CURRENT_BRANCH}"
+fi
+
+submodule_gmqcc_status=$(git submodule status gmqcc | awk '{ print $1 }')
+if ((INIT_SUBMODULES == 1)); then
+  if [[ "${submodule_gmqcc_status:0:1}" == "+" || "${submodule_gmqcc_status:0:1}" == "-" ]]; then
+    echo "=> Submodule gmqcc not initialized... (commit: ${submodule_gmqcc_status})"
+    if ! git submodule update --init $FILTER_FLAGS gmqcc; then
+      echo " -> Failed to initialize gmqcc submodule... aborting"
+      exit 1
+    fi
+  else
+    echo "=> Submodule gmqcc already initialized. (commit: ${submodule_gmqcc_status})"
+  fi
+else
+  echo "=> Skipping gmqcc initialization... (commit: ${submodule_gmqcc_status})"
+fi
+
+if [[ ! -f "gmqcc/gmqcc" ]]; then
+  echo "=> Building gmqcc..."
+  if ! make -C gmqcc -j"$(nproc)"; then
+    echo " -> Failed to build gmqcc... aborting"
+    exit 1
+  fi
+else
+  echo "=> Using existing gmqcc binary (VERSION: $(gmqcc/gmqcc --version))"
+fi
+
+if [[ ! -d "build/${CURRENT_BRANCH}" ]]; then
+  echo "=> Creating build directory... (build/${CURRENT_BRANCH})"
+  if ! mkdir -p "build/${CURRENT_BRANCH}"; then
+    echo " -> Could not create build directory... aborting"
+    exit 1
+  fi
+fi
+
+submodule_xonotic_status=$(git submodule status xonotic | awk '{ print $1 }')
+if ((INIT_SUBMODULES == 1)); then
+  if [[ "${submodule_xonotic_status:0:1}" == "+" || "${submodule_xonotic_status:0:1}" == "-" ]]; then
+    echo "=> Submodule xonotic not initialized... (commit: ${submodule_xonotic_status})"
+    if ! git submodule update --init $FILTER_FLAGS xonotic; then
+      echo " -> Failed to initialize xonotic submodule... aborting"
+      exit 1
+    fi
+  else
+    echo "=> Submodule xonotic already initialized. (commit: ${submodule_xonotic_status})"
+  fi
+else
+  echo "=> Skipping xonotic submodule initialization... (commit: ${submodule_xonotic_status})"
+fi
+
+export XONOTIC=${XONOTIC:-1}
+export QCC=${QCC:-"$(pwd)/gmqcc/gmqcc"}
+
+if ! WATERMARK="$(git describe --tags --dirty='~' --always)"; then
+  echo "-> Error: Failed to generate QCCFLAGS_WATERMARK... aborting"
+  exit 1
+else
+  echo "=> Version tag is: ${WATERMARK}"
+fi
+export QCCFLAGS_WATERMARK="${WATERMARK}"
+export PROGS_OUT=${PROGS_OUT:-"$(pwd)/build/${CURRENT_BRANCH}"}
+
+if ! TMP_MOD="$(realpath --relative-to=xonotic/qcsrc "$(pwd)")"; then
+  echo "-> Error: could not calculate BUILD_MOD... aborting"
+  exit 1
+fi
+
+if [[ -z "$TMP_MOD" ]]; then
+  echo "TMP_MOD is empty! ... aborting"
+  exit 1
+fi
+
+export BUILD_MOD="${TMP_MOD}"
+
+if ((CLEAN_BUILD == 1)); then
+  echo "=> Cleaning old build artifacts in build/${CURRENT_BRANCH}"
+  make -C xonotic clean
+  rm -f "${PROGS_OUT}/"csprogs-*.pk3
+fi
+
+echo "=> Starting Compilation..."
+if ! make -C xonotic "${MAKE_TARGETS[@]}"; then
+  echo " -> Modpack compilation failed... aborting"
+  exit 1
+else
+  if ((TARGET_ALL == 1)); then
+    cp xonotic/qcsrc/csprogs-*.pk3 "${PROGS_OUT}"
+  fi
+  exit 0
+fi
